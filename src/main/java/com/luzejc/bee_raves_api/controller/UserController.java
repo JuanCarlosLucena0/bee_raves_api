33d54e4f0eff7c85871109acb6199e0a26d38899
Juan Carlos <JuanCarlosLucena0@gmail.com>
2026-05-24 21:46:30 +0200

    UserController hecho, arreglar UserService

---
+++ src/main/java/com/luzejc/bee_raves_api/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.luzejc.bee_raves_api.controller;
+
+import com.luzejc.bee_raves_api.entity.User;
+import com.luzejc.bee_raves_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> patchUserById(@PathVariable Long id, @RequestBody User user){
+        User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+}
