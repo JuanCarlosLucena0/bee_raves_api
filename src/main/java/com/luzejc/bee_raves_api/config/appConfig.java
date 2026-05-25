29e1d11a96b8dbaf8aaa2e8b2877ca9bc5d187b1
Juan Carlos <JuanCarlosLucena0@gmail.com>
2026-05-25 19:48:54 +0200

    App config, api funcional con postman

---
+++ src/main/java/com/luzejc/bee_raves_api/config/appConfig.java
@@ -0,0 +1,14 @@
+package com.luzejc.bee_raves_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class appConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
