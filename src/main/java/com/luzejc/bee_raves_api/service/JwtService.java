53908c0ab088302ca551085ee4516a1f3356b7e9
Juan Carlos <JuanCarlosLucena0@gmail.com>
2026-06-15 01:40:24 +0200

    Base para el sistema security implementado

---
+++ src/main/java/com/luzejc/bee_raves_api/service/JwtService.java
@@ -0,0 +1,58 @@
+package com.luzejc.bee_raves_api.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("jwt.expiration")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public boolean isTokenValid(String token){
+        try{
+            getClaims(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public String extractEmail(String token){
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token){
+    return Jwts.parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+
+
+
+}
